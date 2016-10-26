a37660f0a30cda6161d828d0e121b7e1bd785c0a
Matthew Chenoweth <Mchenoweth@MMCI.local>
2016-10-26 16:01:10 -0500

    Initial Commit

---
+++ tposBench01.R
@@ -0,0 +1,18 @@
+#==============================================================================
+#
+#  Two Point Oh System Data Benchmarking R Script
+#  Matthew Chenoweth
+#  2016/10/26
+#
+#  Given expected input from EBS and PMFleet systems, compute the following: 1)
+# Cost Per Unit, 2) Cost Per Hour, 3) Total Expense, and 4) Utilization
+#  Incidentally, Utilize Git to track this project locally, and (hopefully) use
+# Github to publish code. We'll see. The URL _should_ be as follows:
+# https://github.com/mattcheno/TPOSbenchmarking.git
+#
+#------------------------------------------------------------------------------
+
+
+
+#==============================================================================
+#==============================================================================
