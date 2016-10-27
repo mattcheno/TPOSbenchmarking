017f460a30aba9fd4f9a1b3b993bc06328383c8f
Matthew Chenoweth <Mchenoweth@MMCI.local>
2016-10-27 14:14:10 -0500

    Tab data can be imported without errors
    Proof that the PMF tab data can be imported without crazy errors or utf encoding nonsense. This is pre- my getting data from PY which includes customer name fields

--- pmfTabScrubber.R
+++ pmfTabScrubber.R
@@ -20,7 +20,7 @@ working_dir <- getwd()     # Working Directory
 data_dir <- paste(working_dir, "data", sep = "/") # Directory for input data
 pmf_data_file <- paste(data_dir,"pmfTabData.txt", sep = "/") # PMF Data File
   # Column Labels in case Header = False
-pmf_Col_Labels <- c(
+pmf_Col_Names <- c(
   "UnitID",
   "UnitType",
   "Make",
@@ -41,15 +41,15 @@ pmf_Col_Labels <- c(
 )
   # Column Class Overrides for PMF Data
 pmf_Col_Class <- c(
-  NA,     #UnitID
+  "factor",     #UnitID
   NA,     #UnitType
   NA,     #Make
   NA,     #ModelCode
   NA,     #Year
-  "character",     #ServiceDate
+  "Date",     #ServiceDate
   NA,     #UnitAgeAtTimeOfExpense
   NA,     #serviceCategory
-  "character",     #InvoiceDate
+  "Date",     #InvoiceDate
   NA,     #PartsAmount
   NA,     #LaborAmount
   NA,     #MiscAmount
@@ -61,14 +61,27 @@ pmf_Col_Class <- c(
 )
 
 # --- Import Data -------------------------------------------------------------
-pmf_data01 <- read.table(
+pmf_data01 <- read.delim(     # See Note 001
   file = pmf_data_file,
-  header = TRUE,     # first row is header names
+  header = FALSE,     # first row is NOT header names
   sep = "\t",     # tab-delineated
-  colClasses = pmf_Col_Class     # column class overrides
+  col.names = pmf_Col_Names,     # Column Names since header = False
+  colClasses = pmf_Col_Class,     # column class overrides
+  fileEncoding = "UTF-8-BOM"     # See Note 002
 )
+
 
 ##CURSOR
 
 # === FOOTNOTES ===============================================================
+#     Note 001
+# This is read.delim instead of read.table because I kept getting a crazy error
+# when sourcing this code. See the reference for better description and 
+# solution
+# Reference - http://stackoverflow.com/q/26086389
+#     Note 002
+# Using the UTF-8-BOM file encoding gets rid of a funky "ï»¿" I was getting 
+# prepended to the first observation. 
+# Reference - http://stackoverflow.com/q/24568056
+# 
 # === END OF CODE =============================================================
